1faf33c0d8bdb91b9ffe8e4515f0edd7527c379f
Vivek Kumar <patelvivek0222@gmail.com>
2026-09-24 01:27:40 +0530

    register logic

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,15 @@
 import {Router} from "express";
 import {registerUser} from "../controllers/user.controllers.js"
-
+immport {upload} from "../middlewares/multer.middleware.js"
 
 const router=Router()
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    
+    
+    
+    registerUser)
 
 
 export default router
